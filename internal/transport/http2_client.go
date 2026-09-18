15fd5ecb65f3df8dad9a19fb5f95445ebe2db66d
Seal <info@sealsecurity.io>
2026-09-18 16:31:29 +0000

    CVE-2026-84304

--- internal/transport/http2_client.go
+++ internal/transport/http2_client.go
@@ -501,7 +501,7 @@ func (t *http2Client) newStream(ctx context.Context, callHdr *CallHdr, handler s
 		doneFunc:     callHdr.DoneFunc,
 		statsHandler: handler,
 	}
-	s.Stream.buf.init()
+	s.Stream.buf.init(t.bufferPool)
 	s.Stream.wq.init(defaultWriteQuota, s.done)
 	s.readRequester = s
 	// The client side stream context should have exactly the same life cycle with the user provided context.
